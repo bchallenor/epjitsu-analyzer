f5a329f39f7d97ec1f941270b10801d7dee2c889
Ben Challenor <ben@challenor.org>
2013-05-12 14:52:31 +0100

    Also include endpoint

--- src/main/scala/epjitsu/SanePacket.scala
+++ src/main/scala/epjitsu/SanePacket.scala
@@ -5,6 +5,7 @@ import org.joda.time.DateTime
 sealed trait SanePacket extends Packet {
   def direction: UsbXferDir
   def requestId: Long
+  def endpoint: Int
   def bytes: Array[Byte]
 
   private def directionStr = direction match {
@@ -14,14 +15,14 @@ sealed trait SanePacket extends Packet {
 
   private def bytesStr = if (bytes.size <= 72) bytes map ("0x%02x" format _) mkString("{", ", ", "}") else s"${bytes.size} bytes"
 
-  override def toString: String = f"0x$requestId%16x $directionStr $bytesStr"
+  override def toString: String = f"0x$requestId%016x 0x$endpoint%02x $directionStr $bytesStr"
 }
 
-case class SaneSend(timestamp: DateTime, requestId: Long, bytes: Array[Byte]) extends SanePacket {
+case class SaneSend(timestamp: DateTime, requestId: Long, endpoint: Int, bytes: Array[Byte]) extends SanePacket {
   override def direction = UsbOut
 }
 
-case class SaneReceive(timestamp: DateTime, requestId: Long, bytes: Array[Byte]) extends SanePacket {
+case class SaneReceive(timestamp: DateTime, requestId: Long, endpoint: Int, bytes: Array[Byte]) extends SanePacket {
   override def direction = UsbIn
 }
 
@@ -29,14 +30,14 @@ object SanePacketDecoder extends PacketDecoder[(UsbPacket, UsbPacket), SanePacke
   def decode(input: (UsbPacket, UsbPacket)): SanePacket = {
     input match {
       case (
-        UsbPacket(spTs, spId, UsbSubmit, UsbBulk, UsbOut, _, _, _, spBytes),
-        UsbPacket(_, cpId, UsbComplete, UsbBulk, UsbOut, _, _, _, Array())
-      ) if spId == cpId => SaneSend(spTs, spId, spBytes)
+        UsbPacket(spTs, spId, UsbSubmit, UsbBulk, UsbOut, _, _, spEndpoint, spBytes),
+        UsbPacket(_, cpId, UsbComplete, UsbBulk, UsbOut, _, _, cpEndpoint, Array())
+      ) if spId == cpId && spEndpoint == cpEndpoint => SaneSend(spTs, spId, spEndpoint, spBytes)
 
       case (
-        UsbPacket(_, spId, UsbSubmit, UsbBulk, UsbIn, _, _, _, Array()),
-        UsbPacket(cpTs, cpId, UsbComplete, UsbBulk, UsbIn, _, _, _, cpBytes)
-      ) if spId == cpId => SaneReceive(cpTs, cpId, cpBytes)
+        UsbPacket(_, spId, UsbSubmit, UsbBulk, UsbIn, _, _, spEndpoint, Array()),
+        UsbPacket(cpTs, cpId, UsbComplete, UsbBulk, UsbIn, _, _, cpEndpoint, cpBytes)
+      ) if spId == cpId && spEndpoint == cpEndpoint => SaneReceive(cpTs, cpId, cpEndpoint, cpBytes)
 
       case (x, y) => sys.error(f"Could not match ${x.requestId}%16x with ${y.requestId}%16x to make a SanePacket: $x, $y")
     }
